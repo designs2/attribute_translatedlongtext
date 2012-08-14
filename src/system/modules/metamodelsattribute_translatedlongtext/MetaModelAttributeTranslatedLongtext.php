e2d943ae4c7cc85bb28219efd5e8d989fd3fcef7
Christian Schiffler <c.schiffler@cyberspectrum.de>
2012-08-14 18:43:24 +0200

    implemented the missing methods

--- src/system/modules/metamodelsattribute_translatedlongtext/MetaModelAttributeTranslatedLongtext.php
+++ src/system/modules/metamodelsattribute_translatedlongtext/MetaModelAttributeTranslatedLongtext.php
@@ -17,12 +17,14 @@
 
 /**
  * This is the MetaModelAttribute class for handling text fields.
- * 
+ *
  * @package     MetaModels
  * @subpackage  AttributeText
  * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
  */
-class MetaModelAttributeTranslatedLongtext extends MetaModelAttributeComplex
+class MetaModelAttributeTranslatedLongtext
+extends MetaModelAttributeComplex
+implements IMetaModelAttributeTranslated
 {
 
 	public function getAttributeSettingNames()
@@ -84,36 +86,66 @@ class MetaModelAttributeTranslatedLongtext extends MetaModelAttributeComplex
 		return $arrFieldDef;
 	}
 
-	public function parseValue($arrRowData, $strOutputFormat = 'html')
+	public function parseValue($arrRowData, $strOutputFormat = 'text', $objSettings = null)
 	{
 		$arrResult = parent::parseValue($arrRowData, $strOutputFormat);
-		$arrResult['html'] = $arrRowData[$this->getColName()]['value'];
+		$arrResult['text'] = $arrRowData[$this->getColName()]['value'];
 		return $arrResult;
 	}
 
 	public function getDataFor($arrIds)
 	{
-		$objDB = Database::getInstance();
-		$objValue = $objDB->prepare('SELECT * FROM tl_metamodel_translatedlongtext WHERE att_id=? AND langcode=? AND item_id IN (' . implode(',', $arrIds) . ')')
-				->execute($this->get('id'), $this->getMetaModel()->getActiveLanguage());
-		$arrReturn = array();
-		while ($objValue->next())
+		$strActiveLanguage = $this->getMetaModel()->getActiveLanguage();
+		$strFallbackLanguage = $this->getMetaModel()->getFallbackLanguage();
+
+		$arrReturn = $this->getTranslatedDataFor($arrIds, $strActiveLanguage);
+
+		// second round, fetch fallback languages if not all items could be resolved.
+		if ((count($arrReturn) < count($arrIds)) && ($strActiveLanguage != $strFallbackLanguage))
 		{
-			$arrReturn[$objValue->item_id] = $objValue->row();
+			$arrFallbackIds = array();
+			foreach ($arrIds as $intId)
+			{
+				if (empty($arrReturn[$intId]))
+				{
+					$arrFallbackIds[] = $intId;
+				}
+			}
+
+			if ($arrFallbackIds)
+			{
+				$arrFallbackData = $this->getTranslatedDataFor($arrFallbackIds, $strFallbackLanguage);
+				// cannot use array_merge here as it would renumber the keys.
+				foreach ($arrFallbackData as $intId => $arrValue)
+				{
+					$arrReturn[$intId] = $arrValue;
+				}
+			}
 		}
 		return $arrReturn;
 	}
 
 	public function setDataFor($arrValues)
 	{
-		// store to database.
+		foreach ($this->getMetaModel()->getAvailableLanguages() as $strLangCode)
+		{
+			$this->setTranslatedDataFor($arrValues, $strLangCode);
+		}
+	}
+
+	public function unsetDataFor($arrIds)
+	{
+		foreach ($this->getMetaModel()->getAvailableLanguages() as $strLangCode)
+		{
+			$this->unsetValueFor($arrIds, $strLangCode);
+		}
 	}
 
 	/**
 	 * {@inheritdoc}
-	 * 
+	 *
 	 * Fetch filter options from foreign table.
-	 * 
+	 *
 	 */
 	public function getFilterOptions($arrIds = array())
 	{
@@ -133,6 +165,52 @@ class MetaModelAttributeTranslatedLongtext extends MetaModelAttributeComplex
 			$arrReturn[$objValue->value] = $objValue->value;
 		}
 		return $arrReturn;
+	}
+
+	/////////////////////////////////////////////////////////////////
+	// interface IMetaModelAttributeTranslated
+	/////////////////////////////////////////////////////////////////
+
+	public function setTranslatedDataFor($arrValues, $strLangCode)
+	{
+		$strMetaModelTableName = $this->getMetaModel()->getTableName();
+		$arrReturn = array();
+
+		if ($strTableName && $strColNameId)
+		{
+			$strColNameValue = $this->get('select_column');
+			$objDB = Database::getInstance();
+			$strQuery = sprintf('UPDATE %1$s SET %2$s=? WHERE %1$s.id=?', $strMetaModelTableName, $this->getColName());
+			foreach ($arrValues as $intItemId => $arrValue)
+			{
+				$objQuery = $objDB->prepare($strQuery)->execute($arrValue[$strColNameId], $intItemId);
+			}
+		}
+	}
+
+	/**
+	 * Get values for the given items in a certain language.
+	 */
+	public function getTranslatedDataFor($arrIds, $strLangCode)
+	{
+		$objDB = Database::getInstance();
+		$objValue = $objDB->prepare('SELECT * FROM tl_metamodel_translatedlongtext WHERE att_id=? AND langcode=? AND item_id IN (' . implode(',', $arrIds) . ')')
+				->execute($this->get('id'), $strLangCode);
+		$arrReturn = array();
+		while ($objValue->next())
+		{
+			$arrReturn[$objValue->item_id] = $objValue->row();
+		}
+		return $arrReturn;
+	}
+
+	/**
+	 * Remove values for items in a certain lanugage.
+	 */
+	public function unsetValueFor($arrIds, $strLangCode)
+	{
+		$objValue = $objDB->prepare('DELETE FROM tl_metamodel_translatedlongtext WHERE att_id=? AND langcode=? AND item_id IN (' . implode(',', $arrIds) . ')')
+				->execute($this->get('id'), $strLangCode);
 	}
 }
 
